5e494bb5c44da28f0292d4a69784b2238aa68b65
enamchae <56808335+enamchae@users.noreply.github.com>
2025-01-12 18:07:55 -0500

    chore: mobile setup

---
+++ android/app/src/main/java/com/example/streamecommerce/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.streamecommerce;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.streamecommerce',
+  appName: 'stream-ecommerce',
+  webDir: 'build'
+};
+
+export default config;
