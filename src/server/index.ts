873bd63cf9d5398bd46be163facdfa08f873276c
enamchae <56808335+enamchae@users.noreply.github.com>
2025-01-12 20:46:41 -0500

    feat: message sending on localhost

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 
 const server = new WebSocketServer({ port: 8080 });
 
@@ -6,7 +6,14 @@ server.on("connection", (socket) => {
     console.log("Client connected");
 
     socket.on("message", (message) => {
-        console.log(message.toString());
+        const data = JSON.parse(message.toString());
+        console.log(data);
+
+        server.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(data));
+            }
+        });
     });
 });
 console.log("listening");
--- src/stories/Chat.svelte
+++ src/stories/Chat.svelte
@@ -1,12 +1,48 @@
 <script lang="ts">
 import { onMount } from "svelte";
 
+let message = $state("");
+
+let messageHistory = $state<{userId: string, message: string}[]>([]);
+
+const clientName = crypto.randomUUID();
+
+let ws: WebSocket | null = null;
+
 onMount(() => {
-    const ws = new WebSocket(`ws://localhost:8080`);
+    ws = new WebSocket(`ws://localhost:8080`);
+
     ws.addEventListener("open", (event) => {
         console.log("connected");
+    });
 
-        ws.send("ee");
+    ws.addEventListener("message", (event) => {
+        const data = JSON.parse(event.data);
+        console.log(data);
+
+        messageHistory.push(data);
     });
 });
+
+const sendMessage = () => {
+    ws.send(JSON.stringify({
+        userId: clientName,
+        message,
+    }));
+
+    message = "";
+};
+
 </script>
+
+{#each messageHistory as message}
+    <div>
+        {message.userId}: {message.message}
+    </div>
+{/each}
+
+<input
+    type="text"
+    bind:value={message}
+    on:keydown={event => event.key === "Enter" && sendMessage()}
+/>
