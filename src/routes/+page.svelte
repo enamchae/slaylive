b4f6f250c5322dbe200e9af65bb79a13bf554baf
enamchae <56808335+enamchae@users.noreply.github.com>
2025-01-12 19:37:47 -0500

    feat: video capture

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+let video = $state<HTMLVideoElement | null>(null);
+
+const startStream = async () => {
+    video!.srcObject = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+};
+</script>
+
+<div>
+    <button onclick={startStream}>Start</button>
+    <video
+        bind:this={video}
+    autoplay
+    ></video>
+</div>
+
+
+<style lang="scss">
+div {
+    display: flex;
+    flex-direction: column;
+}
+</style>
